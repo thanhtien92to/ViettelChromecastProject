1a3f089ebd0e5be4698af488a720ce71d182fdb8
Nguyen Thanh Tien <nt.tien@alticast.com>
2017-09-19 14:06:38 +0700

    Revert "no message"
    This reverts commit b9098447177ab4a00cc85f4b92326d68b199e36b.

--- castReceiverPlayer.js
+++ castReceiverPlayer.js
@@ -567,7 +567,6 @@ castReceiverPlayer.ChromecastPlayer.prototype.preloadVideo_ = function (mediaInf
 		console.log('updateLicenseRequestInfo --- requestInfo : ' + JSON.stringify(requestInfo));
         requestInfo.headers = {};
 		requestInfo.headers['AUTHORIZATION'] = 'BASIC QWx0aWNhc3Q6RFJNU2VydmljZQ==';
-		requestInfo.headers['host'] = 'sec.viettelttv.com.vn';
 		//requestInfo.headers['Access-Control-Allow-Origin'] = '*';
 		//requestInfo.headers['Origin'] = '*';
     };/**/
@@ -870,7 +869,6 @@ castReceiverPlayer.ChromecastPlayer.prototype.loadVideo_ = function (info) {
 				console.log('updateLicenseRequestInfo --- requestInfo : ' + JSON.stringify(requestInfo));
 				requestInfo.headers = {};
 				requestInfo.headers['AUTHORIZATION'] = 'BASIC QWx0aWNhc3Q6RFJNU2VydmljZQ==';
-				requestInfo.headers['host'] = 'sec.viettelttv.com.vn';
 				//requestInfo.headers['Access-Control-Allow-Origin'] = '*';
 				//requestInfo.headers['Origin'] = '*';
 			};/**/
@@ -1042,7 +1040,6 @@ castReceiverPlayer.ChromecastPlayer.prototype.processTtmlCues_ =
 					console.log('updateLicenseRequestInfo --- requestInfo : ' + JSON.stringify(requestInfo));
 					requestInfo.headers = {};
 					requestInfo.headers['AUTHORIZATION'] = 'BASIC QWx0aWNhc3Q6RFJNU2VydmljZQ==';
-					requestInfo.headers['host'] = 'sec.viettelttv.com.vn';
 					//requestInfo.headers['Access-Control-Allow-Origin'] = '*';
 					//requestInfo.headers['Origin'] = '*';
 				};/**/
